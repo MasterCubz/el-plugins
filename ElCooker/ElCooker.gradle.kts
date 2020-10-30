dbd7bcfdb087774eeb53a64c7f8882b4df359ff8
Elli-tt <elliottjoiner@me.com>
2020-10-30 17:25:51 +0000

    global: version bump for bootstrap

--- ElCooker/ElCooker.gradle.kts
+++ ElCooker/ElCooker.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "2.1.2"
+version = "2.1.3"
 
 project.extra["PluginName"] = "El Cooker" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "Cooks raw food" // This is the description that is used in the external plugin manager panel
